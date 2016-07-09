8fc392886d347df34e1fdaae51861640afb9fac5
Cezary Piątek <cepi@ailleron.com>
2016-07-10 00:16:37 +0200

    Update gulpfile.js - fix jquery.unobtrusive-ajax.js file name - add uglify-save-license module - regenerate jquery.unobtrusive-ajax.min.js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require("gulp"),
+    saveLicense = require('uglify-save-license'),
     uglify = require("gulp-uglify"),
     rename = require('gulp-rename');
 
 gulp.task("minifyJS", function () {
-    gulp.src(["jquery-ajax-unobtrusive.js"], { base: "." })
-        .pipe(uglify())
+    gulp.src(["jquery.unobtrusive-ajax.js"], { base: "." })
+        .pipe(uglify({
+            preserveComments: saveLicense
+        }))
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest("."));
 });
