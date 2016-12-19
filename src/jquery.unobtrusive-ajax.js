c48f44fd44827f1490497638fb7a0a59a2c29ff5
Kiran Challa <kichalla@microsoft.com>
2016-12-19 13:45:11 -0800

    Updated gulp file to add version to license header Related to https://github.com/aspnet/jquery-validation-unobtrusive/issues/51

--- src/jquery.unobtrusive-ajax.js
+++ src/jquery.unobtrusive-ajax.js
@@ -1,18 +1,17 @@
-/* 
- * Microsoft grants you the right to use these script files for the sole
- * purpose of either: (i) interacting through your browser with the Microsoft
- * website or online service, subject to the applicable licensing or use
- * terms; or (ii) using the files as included with a Microsoft product subject
- * to that product's license terms. Microsoft reserves all other rights to the
- * files not expressly granted by Microsoft, whether by implication, estoppel
- * or otherwise. Insofar as a script file is dual licensed under GPL,
- * Microsoft neither took the code under GPL nor distributes it thereunder but
- * under the terms set out in this paragraph. All notices and licenses
- * below are for informational purposes only.
-/*!
-** Unobtrusive Ajax support library for jQuery
-** Copyright (C) Microsoft Corporation. All rights reserved.
-*/
+// Unobtrusive Ajax support library for jQuery
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// @version <placeholder>
+// 
+// Microsoft grants you the right to use these script files for the sole
+// purpose of either: (i) interacting through your browser with the Microsoft
+// website or online service, subject to the applicable licensing or use
+// terms; or (ii) using the files as included with a Microsoft product subject
+// to that product's license terms. Microsoft reserves all other rights to the
+// files not expressly granted by Microsoft, whether by implication, estoppel
+// or otherwise. Insofar as a script file is dual licensed under GPL,
+// Microsoft neither took the code under GPL nor distributes it thereunder but
+// under the terms set out in this paragraph. All notices and licenses
+// below are for informational purposes only.
 
 /*jslint white: true, browser: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: false */
 /*global window: false, jQuery: false */
