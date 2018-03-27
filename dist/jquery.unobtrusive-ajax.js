4269c0f874836ace88810e645a82fa9ca75697e9
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2018-03-26 17:19:25 -0700

    Use jQuery apis for handling before and after insertion modes

--- dist/jquery.unobtrusive-ajax.js
+++ dist/jquery.unobtrusive-ajax.js
@@ -55,23 +55,18 @@
             var top;
 
             switch (mode) {
-            case "BEFORE":
-                top = update.firstChild;
-                $("<div />").html(data).contents().each(function () {
-                    update.insertBefore(this, top);
-                });
-                break;
-            case "AFTER":
-                $("<div />").html(data).contents().each(function () {
-                    update.appendChild(this);
-                });
-                break;
-            case "REPLACE-WITH":
-                $(update).replaceWith(data);
-                break;
-            default:
-                $(update).html(data);
-                break;
+                case "BEFORE":
+                    $(update).prepend(data);
+                    break;
+                case "AFTER":
+                    $(update).append(data);
+                    break;
+                case "REPLACE-WITH":
+                    $(update).replaceWith(data);
+                    break;
+                default:
+                    $(update).html(data);
+                    break;
             }
         });
     }
